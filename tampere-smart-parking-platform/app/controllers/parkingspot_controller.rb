3b3c8ffd3aa525ce1db3627c87ee35650e052596
Felix Seidel <felix@seidel.me>
2018-11-23 22:38:35 +0200

    Parkingspot: add create action

--- tampere-smart-parking-platform/app/controllers/parkingspot_controller.rb
+++ tampere-smart-parking-platform/app/controllers/parkingspot_controller.rb
@@ -3,4 +3,21 @@ class ParkingspotController < ApplicationController
     @latest_parkingspot = Parkingspot.last
     render :json => @parkingspot
   end
+
+  def create
+    parkingspot = Parkingspot.create(parkingspot_params)
+
+    if parkingspot.valid?
+      parkingspot.save!
+      head :ok
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  private
+
+  def parkingspot_params
+    params.require(:parkingspot).permit(:latitude, :longitude)
+  end
 end
