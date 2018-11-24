bc808e6e409eb183f50a975c135ed893acff5b98
pantajosef <pantazidisjosef@hotmail.de>
2018-11-24 03:15:10 +0100

    capture contours when capturing image

--- image-capturing/capture_webcam_image.py
+++ image-capturing/capture_webcam_image.py
@@ -1,8 +1,17 @@
+from shapedetector import ShapeDetector
+import argparse
+import imutils
 import cv2
+
+def color_for_shape(shabe):
+  if shabe == "triangle":
+    return (0, 255, 0)
+  return (0, 0, 255)
 
 cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("CaptureImage")
+# cv2.namedWindow("DetectedImage")
 
 img_counter = 0
 
@@ -20,10 +29,32 @@ while True:
     break
   elif k%256 == 32:
     # SPACE pressed
-    img_name = "opencv_frame_{}.png".format(img_counter)
-    cv2.imwrite(img_name, frame)
-    print("Image {} captured!".format(img_name))
-    img_counter += 1
+    image = frame
+    resized = imutils.resize(image, width=300)
+    ratio = image.shape[0] / float(resized.shape[0])
+
+    # convert the resized image to grayscale, blur it slightly,
+    # and threshold it
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+    # find contours in the thresholded image and initialize the
+    # shape detector
+    contours = cv2.findContours(thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if imutils.is_cv2() else contours[1]
+    shape_detector = ShapeDetector()
+
+    for contour in contours:
+      shape = shape_detector.detect(contour)
+      if shape in ["triangle", "rectangle", "square"]:
+        color = color_for_shape(shape)
+        contour = contour.astype("float")
+        contour *= ratio
+        contour = contour.astype("int")
+        cv2.drawContours(image, [contour], -1, color, 2)
+        cv2.imshow("CaptureImage", image)
+    
+    cv2.waitKey(0)
 
 cam.release()
 
--- image-capturing/detect_shapes.py
+++ image-capturing/detect_shapes.py
@@ -2,15 +2,14 @@
 # python detect_shapes.py --image shapes_and_colors.png
 
 # import the necessary packages
-from pyimagesearch.shapedetector import ShapeDetector
+from .shapedetector import ShapeDetector
 import argparse
 import imutils
 import cv2
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to the input image")
+ap.add_argument("-i", "--image", required=True, help="path to the input image")
 args = vars(ap.parse_args())
 
 # load the image and resize it to a smaller factor so that
@@ -31,6 +30,8 @@ cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 sd = ShapeDetector()
+
+
 
 # loop over the contours
 for c in cnts:
