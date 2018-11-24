f50dcc88619275522cc96810378611d6c938f1d6
Felix Seidel <felix@seidel.me>
2018-11-24 02:42:58 +0200

    Add DialogFlowController

--- tampere-smart-parking-platform/config/routes.rb
+++ tampere-smart-parking-platform/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :parking_spots, only: [:create, :index]
 
+  post '/dialog_flow', to: 'dialog_flow#webhook'
+
   get '/.well-known/health_check', to: 'health#health_check'
 end
